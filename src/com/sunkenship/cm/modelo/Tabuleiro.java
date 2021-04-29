dd5a4324c41759d95b56b1735977846c145079dd
Tiago Nunes <ntiagon@gmail.com>
2021-04-29 09:52:25 -0300

    final objetive imp

--- src/com/sunkenship/cm/modelo/Tabuleiro.java
+++ src/com/sunkenship/cm/modelo/Tabuleiro.java
@@ -2,6 +2,7 @@ package com.sunkenship.cm.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
     private int linhas;
@@ -22,14 +23,31 @@ public class Tabuleiro {
     private void gerarCampos() {
         for (int linha = 0; linha < linhas; linha++) {
             for (int coluna = 0; coluna < colunas; coluna++) {
-                campos.add(new Campo(linha,coluna));
+                campos.add(new Campo(linha, coluna));
             }
         }
     }
 
     private void associarOsVizinhos() {
+        for (Campo c1 : campos) {
+            for (Campo c2 : campos) {
+                c1.adicionarVizinho(c2);
+            }
+        }
     }
 
     private void sortearMinas() {
+        long minasArmadas = 0;
+        Predicate<Campo> minado = c -> c.isMinado();
+
+        do {
+            minasArmadas = campos.stream().filter(minado).count();
+            int aleatorio = (int) (Math.random() * campos.size());
+            campos.get(aleatorio).minar();
+        } while (minasArmadas < minas);
+    }
+
+    public boolean objetivoAlcancado() {
+        return campos.stream().allMatch(c -> c.objetivoAlcancado());
     }
 }
